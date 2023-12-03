9ce9f48aaa9ba757e5cd7cf2f1a660110f318cb5
FH.Dev <felixpoletti5@gmail.com>
2023-12-03 17:16:45 +0100

    Update page.tsx

---
+++ app/src/app/post/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/navbar'
+
+const Post: React.FC = () => {
+    return (
+        <main>
+            <Navbar/>
+            <div className="flex justify-between">
+                <div>Post</div>
+            </div>
+        </main>
+    )
+}
+export default Post;
