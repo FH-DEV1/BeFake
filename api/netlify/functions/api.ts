a1ecd7c79d5b07dc103b384f895a68d58dae7886
FH.Dev <felixpoletti5@gmail.com>
2024-03-11 22:47:04 +0100

    CORS fix attempt 2

--- api/netlify/functions/api.ts
+++ api/netlify/functions/api.ts
@@ -1,27 +1,34 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import cors from 'cors';
 import { refreshToken } from '../../routes/refresh.ts';
 import { verifyOTP } from '../../routes/verifyOTP.ts';
 
 const api = express();
 const router = Router();
 
-router.get("/refresh", refreshToken, (req, res) =>  {
-  res.json({
+api.use(cors({
+ origin: 'https://bereal-fhdev.vercel.app',
+ methods: ['GET'],
+//  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+router.get("/refresh", refreshToken, (req, res) => {
+ res.json({
       token: res.locals.token,
       refresh_token: res.locals.refresh_token,
       token_expiration: res.locals.token_expiration
-  });
+ });
 });
 
-router.get("/verify-otp", verifyOTP, (req, res) =>  {
-  res.json({
+router.get("/verify-otp", verifyOTP, (req, res) => {
+ res.json({
     refresh_data: {
       token: res.locals.access_token,
       refresh_token: res.locals.access_refresh_token,
       token_expiration: res.locals.access_expiration
     }
-  });
+ });
 });
 
 api.use("/api/", router);
