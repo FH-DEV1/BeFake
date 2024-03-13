bf3a52f7b19c315c52aa3448e6474c67b369119a
FH.Dev <felixpoletti5@gmail.com>
2024-03-13 20:54:20 +0100

    feed endpoint debug

--- api/routes/feed.ts
+++ api/routes/feed.ts
@@ -8,24 +8,32 @@ export const getFeed = async (req: Request, res: Response, next: NextFunction) =
     try {
         let refreshData: refreshDataType | undefined;
         let token: string | undefined = req.headers.token as string;
-        let tokenExpiration: string | undefined = req.headers.token_expiration as string;
-        let refreshToken: string | undefined = req.headers.refresh_token as string;
+        let token_expiration: string | undefined = req.headers.token_expiration as string;
+        let refresh_token: string | undefined = req.headers.refresh_token as string;
         let userId: string | undefined = req.headers.userId as string;
 
-        if (token && tokenExpiration && refreshToken && userId) {
+        if (token && token_expiration && refresh_token && userId) {
             const now = Date.now();
-            if (now > parseInt(tokenExpiration)) {
+            if (now > parseInt(token_expiration)) {
                 const response = await axios.get(`${domain}/api/refresh`, {
                     headers: {
-                        refresh_token: refreshToken
+                        refresh_token: refresh_token
                     }
                 });
                 refreshData = response.data;
                 token = response.data.token;
             }
-        } else {
-            return res.status(400).json({ error: `Error: token, token_expiration, refresh_token, or userId is undefined` });
-        }
+        } else if (!token) {
+                return res.status(400).json({ error: `Error: token is undefined` });
+            } else if (!token_expiration) {
+                return res.status(400).json({ error: `Error: token_expiration is undefined` });
+            } else if (!refresh_token) {
+                return res.status(400).json({ error: `Error: refresh_token is undefined` });
+            } else if (!userId) {
+                return res.status(400).json({ error: `Error: userId is undefined` });
+            } else {
+                return res.status(400).json({ error: `Error: Impossible error` });
+            }
 
         const feedResponse = await axios.get('https://mobile.bereal.com/api/feeds/friends-v1', {
             headers: {
