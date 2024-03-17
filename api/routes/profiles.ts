62c458a0ae55cab6732ba3412f783e443ddfce78
FH.Dev <felixpoletti5@gmail.com>
2024-03-17 12:36:48 +0100

    fixed profiles on API

--- api/routes/profiles.ts
+++ api/routes/profiles.ts
@@ -1,32 +1,60 @@
 import { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
+import { refreshDataType } from '../types/Types';
+
+const domain = process.env.DOMAIN
 
 export const profiles = async (req: Request, res: Response, next: NextFunction) => {
-    // Retrieve the token from the request headers
-    let token: string|null = req.headers.token;
-    let userId: string|null = req.headers.userId;
-
     try {
-        if (token) {
-            // Make a GET request to fetch user profile data using the token and user ID
-            const response = await axios.get(`https://mobile.bereal.com/api/person/profiles/${userId}`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                    'bereal-app-version-code': '14549',
-                    'bereal-signature': 'MToxNzA3NDgwMjI4OvR2hbFOdgnyAz1bfiCp68ul5sVZiHnv+NAZNySEcBfD',
-                    'bereal-timezone': 'Europe/Paris',
-                    'bereal-device-id': '937v3jb942b0h6u9'
-                }
-            })
+        let refreshData: refreshDataType | undefined;
+        let token: string | undefined = req.headers.token as string;
+        let token_expiration: string | undefined = req.headers.token_expiration as string;
+        let refresh_token: string | undefined = req.headers.refresh_token as string;
+        let userId: string | undefined = req.headers.userid as string;
 
-            res.locals.response = { data: response.data};
+        if (token && token_expiration && refresh_token && userId) {
+            const now = Date.now();
+            if (now > parseInt(token_expiration)) {
+                const response = await axios.get(`${domain}/api/refresh`, {
+                    headers: {
+                        refresh_token: refresh_token
+                    }
+                });
+                refreshData = response.data;
+                token = response.data.token;
+            }
+        } else if (!token) {
+            return res.status(400).json({ error: `Error: token is undefined` });
+        } else if (!token_expiration) {
+            return res.status(400).json({ error: `Error: token_expiration is undefined` });
+        } else if (!refresh_token) {
+            return res.status(400).json({ error: `Error: refresh_token is undefined` });
+        } else if (!userId) {
+            return res.status(400).json({ error: `Error: userId is undefined` });
+        } else {
+            return res.status(400).json({ error: `Error: Impossible error` });
+        }
+
+        const response = await axios.get(`https://mobile.bereal.com/api/person/profiles/${userId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'bereal-app-version-code': '14549',
+                'bereal-signature': 'MToxNzA3NDgwMjI4OvR2hbFOdgnyAz1bfiCp68ul5sVZiHnv+NAZNySEcBfD',
+                'bereal-timezone': 'Europe/Paris',
+                'bereal-device-id': '937v3jb942b0h6u9'
+            }
+        })
+
+        if (response.data) {
+            if (refreshData) {
+                res.locals.response = {data: response.data, refreshData: refreshData}
+            } else {
+                res.locals.response = {data: response.data}
+            }
 
             return next();
-        } else {
-            return res.status(400).json({ error: `Error: token, token_expiration, refresh_token, or userId is undefined` });
         }
     } catch (error: any) {
-        // Return an error response if the request fails
         if (error.response.data) {
             return res.status(400).json({ error: error.response.data });
         } else {
